ec56bb18f111e86abae8bd0003add38921561e1d
Ilia Belokopytov <thecapitalB@yandex.ru>
2021-03-08 23:05:00 +0300

    feat: all in

--- scripts/index.js
+++ scripts/index.js
@@ -89,7 +89,11 @@ function duplicateTaskHandler(evt) {
 
 	//вставить на страницу
 	boxCards.prepend(newTask);
+
+
 }
+
+
 
 function addTaskListeners(task) {
 	const deleteButton = task.querySelector('.element__btn_delete');
@@ -103,6 +107,17 @@ function createTaskDomNode(item){
 	title.textContent = item.name;
   const link = newItem.querySelector('#cardLink');
   link.src = item.link;
+
+  link.addEventListener('click',function showPopupImg() {
+    bigImg.classList.toggle('popup-image_is-opened');
+    const titleImg = document.querySelector('.popup-image__title');
+    const popupImg = document.querySelector('.popup-image__image');
+    const closeImg = document.querySelector('.popup-image__close');
+    titleImg.textContent = title.textContent;
+    popupImg.src = link.src;
+
+    closeImg.addEventListener('click', closePopupImg);
+  });
 	return newItem;
 }
 
@@ -110,10 +125,17 @@ function renderList() {
 	const result = initialCards.map(function(item) {
 		const newTask = createTaskDomNode(item);
 		addTaskListeners(newTask);
+
+    newTask.querySelector('.element__like').addEventListener('click', function (evt) {
+    evt.target.classList.toggle('element__like_active');
+    });
 		return newTask;
+
+
 	});
 
 	boxCards.append(...result);
+
 }
 
 renderList();
@@ -130,94 +152,29 @@ boxCardsForm.addEventListener('click', showPopupCard);
 
 function addTaskFormListener(event) {
 	event.preventDefault();
-	const input = document.querySelector('#cardTitle');
+	const input = document.querySelector('#imputTitle');
 	const inputTitle = input.value;
-
-	const newTask = createTaskDomNode({ name: inputTitle });
+  const input2 = document.querySelector('#imputLink');
+	const inputLink = input2.value;
+	const newTask = createTaskDomNode({ name: inputTitle, link: inputLink });
 
 	addTaskListeners(newTask);
 
 	boxCards.prepend(newTask);
 
 	input.value = '';
+  input2.src = '';
+  closePopupCard();
 }
 seveCardBotton.addEventListener('submit', addTaskFormListener);
 closepopupCard.addEventListener('click', closePopupCard);
 
 
-// const TODO_LIST = [
-// 	{ title: 'Съесть печеньку 🍪' },
-// 	{ title: 'Изучить React ⚛️' },
-// 	{ title: 'Полить цветы 🌸' },
-// 	{ title: 'Сделать свой сайт 👨‍💻' },
-// 	{ title: 'Погулять в парке 🌳' },
-// ];
 
-// const container = document.querySelector('.todo__list');
-// const todoForm = document.querySelector('.todo__form');
-// const templateElement = document.querySelector('.template');
-
-// function deleteTaskHandler(evt) {
-// 	const target = evt.target;
-// 	const currentTask = target.closest('.task');
-
-// 	currentTask.remove();
-// }
-
-// function duplicateTaskHandler(evt) {
-// 	//найти дом ноду задачи
-// 	const target = evt.target;
-// 	const currentTask = target.closest('.task');
-
-// 	//создать новую дом ноду задачи передав найденный текст
-// 	const newTask = currentTask.cloneNode(true);
-
-// 	//навесить на новую задачу обработчики события
-// 	addTaskListeners(newTask);
-
-// 	//вставить на страницу
-// 	container.prepend(newTask);
-// }
+const bigImg = document.querySelector('.popup-image');
+const bigImgclose = document.querySelector('.popup-image__close');
 
 
-// function addTaskListeners(task) {
-// 	const deleteButton = task.querySelector('.task__btn_delete');
-// 	deleteButton.addEventListener('click', deleteTaskHandler);
-
-// 	const duplicateButton = task.querySelector('.task__btn_copy');
-// 	duplicateButton.addEventListener('click', duplicateTaskHandler);
-// }
-
-// function createTaskDomNode(item){
-// 	const newItem = templateElement.content.cloneNode(true);
-// 	const title = newItem.querySelector('.task__name');
-// 	title.textContent = item.title;
-
-// 	return newItem;
-// }
-
-// function renderList() {
-// 	const result = TODO_LIST.map(function(item) {
-// 		const newTask = createTaskDomNode(item);
-// 		addTaskListeners(newTask);
-// 		return newTask;
-// 	});
-
-// 	container.append(...result);
-// }
-
-// function addTaskFormListener(evt) {
-// 	evt.preventDefault();
-// 	const input = todoForm.querySelector('.todo__input');
-// 	const inputTitle = input.value;
-
-// 	const newTask = createTaskDomNode({ title: inputTitle });
-
-// 	addTaskListeners(newTask);
-
-// 	container.prepend(newTask);
-
-// 	input.value = '';
-// }
-
-// todoForm.addEventListener('submit', addTaskFormListener);
+function closePopupImg() {
+  bigImg.classList.toggle('popup-image_is-opened');
+}
