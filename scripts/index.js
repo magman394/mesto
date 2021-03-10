af7e6f0cc1b8f30fa4b9f31550224deb23182997
Ilia Belokopytov <thecapitalB@yandex.ru>
2021-03-11 00:29:10 +0300

    fix: все кроме думнода

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,19 @@
-let showFormBotton = document.querySelector('.profile__edit-botton')
-let showForm = document.querySelector('.popup');
-let closeFormBotton = document.querySelector('.popup__close');
-let seveBotton = document.querySelector('#save')
-let firstNameContainer = document.querySelector('.profile__name');
-let lastNameContainer = document.querySelector('.profile__profession');
-let firstName = document.querySelector('#firstName');
-let lastName = document.querySelector('#lastName');
-let formLissener = document.querySelector('form');
 
-function showPopup() {
-  firstName.value = firstNameContainer.textContent
-  lastName.value = lastNameContainer.textContent
-  showForm.classList.toggle('popup_is-opened');
-}
+const seveBotton = document.querySelector('#save')
+const firstNameContainer = document.querySelector('.profile__name');
+const lastNameContainer = document.querySelector('.profile__profession');
+const firstName = document.querySelector('#firstName');
+const lastName = document.querySelector('#lastName');
+const formLissener = document.querySelector('form');
+// точно нужны
+const bigImgclose = document.querySelector('.popup__close_image');
+const closeImg = document.querySelector('.popup__close_image');
+const closeFormBotton = document.querySelector('.popup__close');
+const popup = document.querySelector('.popup');
+const showFormBotton = document.querySelector('.profile__edit-botton');
+const showForm = document.querySelector('#popupAutor');
 
-function closePopup() {
-  showForm.classList.toggle('popup_is-opened');
-}
+
 
 
 function addName(event) {
@@ -25,12 +22,155 @@ function addName(event) {
   lastNameContainer.textContent = lastName.value;
     firstName.value = '';
     lastName.value = '';
-    showForm.classList.toggle(closePopup());
-
+    showForm.classList.toggle(closePopup(popup));
+    firstName.value = firstNameContainer.textContent
+    lastName.value = lastNameContainer.textContent
 }
 
-closeFormBotton.addEventListener('click', closePopup);
-showFormBotton.addEventListener('click', showPopup);
+
 
 formLissener.addEventListener('submit', addName);
 
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const boxCards = document.querySelector('.elements');
+const boxCardsForm = document.querySelector('.profile__add-botton');
+const templateElement = document.querySelector('#boxCards');
+const showpopupCard = document.querySelector('#popupCard');
+const seveCardBotton = document.querySelector('#formCards')
+const closepopupCard = document.querySelector('#closeCard');
+
+function deleteCardHandler(evt) {
+	const target = evt.target;
+	const currentTask = target.closest('#cardElement');
+	currentTask.remove();
+}
+
+
+function addTaskListeners(task) {
+	const deleteButton = task.querySelector('.element__btn_delete');
+	deleteButton.addEventListener('click', deleteCardHandler);
+
+}
+const bigImg = document.querySelector('#popupImage');
+function createTaskDomNode(item){
+	const newItem = templateElement.content.cloneNode(true);
+	const title = newItem.querySelector('#cardTitle');
+	title.textContent = item.name;
+  const link = newItem.querySelector('#cardLink');
+  link.src = item.link;
+  link.alt = item.name;
+
+
+  link.addEventListener('click', function () {
+    bigImg.classList.toggle('popup_is-opened');
+    const titleImg = document.querySelector('.popup__title');
+    const popupImg = document.querySelector('.popup__image');
+    titleImg.textContent = title.textContent;
+    popupImg.src = link.src;
+    popupImg.alt = title.textContent;
+
+    closeImg.addEventListener('click', closePopup(popup));
+  });
+	return newItem;
+}
+
+function renderList() {
+	const result = initialCards.map(function(item) {
+		const newTask = createTaskDomNode(item);
+		addTaskListeners(newTask);
+
+    newTask.querySelector('.element__like').addEventListener('click', function (evt) {
+    evt.target.classList.toggle('element__like_active');
+    });
+		return newTask;
+
+
+	});
+
+	boxCards.append(...result);
+
+}
+
+renderList();
+
+
+
+
+
+
+function cardFormSubmitHandler(event) {
+	event.preventDefault();
+	const cardTitleInputValue = document.querySelector('#inputTitle');
+	const inputTitle = cardTitleInputValue.value;
+  const cardLinkInputValue = document.querySelector('#inputLink');
+	const inputLink = cardLinkInputValue.value;
+	const newTask = createTaskDomNode({ name: inputTitle, link: inputLink });
+
+	addTaskListeners(newTask);
+  newTask.querySelector('.element__like').addEventListener('click', function (evt) {
+    evt.target.classList.toggle('element__like_active');
+    });
+	boxCards.prepend(newTask);
+
+	cardTitleInputValue.value = '';
+  cardLinkInputValue.value = '';
+  closePopup(showpopupCard);
+}
+
+seveCardBotton.addEventListener('submit', cardFormSubmitHandler);
+
+
+
+function showPopup(popup) {
+  popup.classList.add('popup_is-opened');
+}
+
+function closePopup(popup) {
+  popup.classList.remove('popup_is-opened');
+}
+
+showFormBotton.addEventListener('click', function () {
+  showPopup(showForm)
+});
+
+boxCardsForm.addEventListener('click', function () {
+  showPopup(showpopupCard)
+});
+
+closeFormBotton.addEventListener('click', function () {
+  closePopup(showForm)
+});
+
+closepopupCard.addEventListener('click', function () {
+  closePopup(showpopupCard)
+});
+bigImgclose.addEventListener('click', function () {
+  closePopup(bigImg)
+});
