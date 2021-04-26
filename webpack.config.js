1919af2bcc09a66ca0cbea90ffd23a26483f722b
Ilia Belokopytov <thecapitalB@yandex.ru>
2021-04-26 19:52:43 +0300

    feat: удалил нод

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,53 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/scripts/index.js',
-  target: 'es5',
+  entry: {
+    main: './src/scripts/index.js'
+  },
   output: {
-    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: '',
+  },
+  mode: 'development',
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist'),
+    open: true,
+    compress: true,
+    port: 8080
   },
   module: {
-    rules: [
-    {
-      test: /\.m?js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: 'babel-loader'
-        
-      }
-    },
+    rules: [{
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: '/node_modules/'
+      },
       {
-        test: /\.css$/i,
-         use: [MiniCssExtractPlugin.loader,
-          {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, {
             loader: 'css-loader',
             options: {
-              importLoaders: 1,
-            },
-          },
-          'postcss-loader'
-        ],
-
-  }, {
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-            	esModule: false
+              importLoaders: 1
             }
           },
-        ],
+          'postcss-loader'
+        ]
       },
-    ],
-  },
-    plugins: [
-    new HtmlWebpackPlugin({
-    	filename: 'index.html',
-    	template: './src/index.html'
-    	}),
-    new MiniCssExtractPlugin()
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
 
-};
+  ]
+}
