d3ef7f8237e51f26de09a2fee01c56e944c342e4
Ilia Belokopytov <thecapitalB@yandex.ru>
2021-04-26 21:22:33 +0300

    feat: настройка вебпака 2

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,17 @@
-const autoprefixer = require('autoprefixer')
-const cssnano = require('cssnano')
+// postcss.config.js
+
+// подключите плагины в файл
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
 module.exports = {
+  // подключите плагины к PostCSS
   plugins: [
-  	cssnano({
-            preset: 'default',
-        }),
-    autoprefixer()
-  ],
+    // подключите autoprefixer
+    autoprefixer,
+    // cssnano при подключении нужно передать объект опций
+    // { preset: default } говорит о том, что нужно использовать
+    // стандартные настройки минификации
+    cssnano({ preset: 'default' })
+  ]
 };
